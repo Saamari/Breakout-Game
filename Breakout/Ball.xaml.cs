853360b271a930ec23ce77305a467a953f5e3036
Saamari <sampsa_kautto@hotmail.com>
2016-03-22 10:36:45 +0200

    peli

---
+++ Breakout/Ball.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Breakout
+{
+    public sealed partial class Ball : UserControl
+    {
+
+            //location
+            public double LocationX { get; set; }
+            public double LocationY { get; set; }
+            //speed
+            public double SpeedX { get; set; }
+            public double SpeedY { get; set; }
+
+
+
+        public Ball()
+        {
+            this.InitializeComponent();
+
+            Width = 15;
+            Height = 15;
+            SpeedX = -4;  //random
+            SpeedY = -4;
+        }
+
+        // return balls rect
+        public Rect GetRect()
+        {
+            return new Rect(LocationX, LocationY, Width, Height);
+        }
+
+        //move ball
+        public void Move()
+        {
+            //change in x
+            LocationX = LocationX + SpeedX;
+            if (LocationX < 0)
+            {
+                LocationX = 0;
+                SpeedX *= -1;
+            }else if(LocationX + Width > MainPage.CanvasWidth) // RIGHT
+            {
+                LocationX = MainPage.CanvasWidth = Width;
+                SpeedX *= -1;
+            }
+
+            //c´Change in y
+            LocationY = LocationY + SpeedY;
+            if (LocationY < 0) // Up
+            {
+                LocationY = 0;
+                SpeedY *= -1;
+            }
+            //move ball in canvas
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+          
+        }
+
+        //ball speed, hitPercent -0.5 <-> 0.5
+        public void SetSpeed(double hitPercent)
+        {
+            SpeedX = hitPercent * 10; // -5 <-> 5 
+            SpeedY *= -1;
+        }
+
+
+    }
+}
---
+++ Breakout/Game.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Breakout
+{
+    class Game
+    {
+        //timer
+        private DispatcherTimer timer;
+        //canvas
+        private Canvas canvas;
+        //Ball
+        private Ball ball;
+        //paddle
+        public Paddle paddle;
+        private List<Block> blocks;
+
+        //Constructor
+        public Game (Canvas canvas)
+        {
+            this.canvas = canvas;
+            CreateBall();
+            CreatePaddle();
+            CreateBlocks();
+        }
+
+        //add ball to game
+        private void CreateBall()
+        {
+            ball = new Ball { LocationX = 390, LocationY = 500 };
+            canvas.Children.Add(ball);                      
+        }
+
+        //add paddle to game
+        private void CreatePaddle()
+        {
+            paddle = new Paddle { LocationX = 350, LocationY = 550 };
+            canvas.Children.Add(paddle);
+        }
+
+        //create blocks
+        private void CreateBlocks()
+        {
+            blocks = new List<Block>();
+            int blocksCount = 120;
+            int cols = 12;
+            int xStartPos = 70;
+            int yStartPos = 30;
+            int step = 5;
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < blocksCount; i++)
+            {
+                //is it a new row
+                if (i % cols ==0 && i > 0)
+                {
+                    row++;
+                        col = 0;
+                }
+                else if (i > 0 )
+                {
+                    col++;
+                }
+
+                //block position
+                int x = (50 + step) * col + xStartPos; // 0, 55, 110...
+                int y = (20 + step) * row + yStartPos; // 0, 25, 50 , 75...
+                // create a block
+                Block block = new Block { LocationX = x, LocationY = y };
+                //add blocks
+                blocks.Add(block); // List-collection
+                //add to canvas
+                canvas.Children.Add(block);
+                //set location in canvas
+                block.SetLocation();
+            }
+        }
+
+        //start game
+        public void StartGame()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60); // 60 fps
+            timer.Start();
+        }
+
+        //game  loop
+        private void Timer_Tick(object sender, object e)
+        {
+
+            ball.Move();
+            CheckCollision();
+            IsGameOver();
+        }
+
+        private void IsGameOver()
+        {
+            //all blocks removed -- WIN
+            if(blocks.Count == 0)
+            {
+                Debug.WriteLine("Game over - Good Job!");
+                timer.Stop();
+            }
+            //ball below paddle
+            if (ball.LocationY > paddle.LocationY)
+            {
+                Debug.WriteLine("Game over - You Lost!");
+                timer.Stop();
+            }
+        }
+
+        private void CheckCollision()
+        {
+            //ball and paddle
+            Rect rect = ball.GetRect();
+            rect.Intersect(paddle.GetRect());
+            if (!rect.IsEmpty)
+            {
+                // paddle 100 px
+                // ball position 0-100
+                double ballPosition = ball.LocationX - paddle.LocationX;
+                // -0.5 <-> 0.5
+                double hitPercent = (ballPosition / paddle.Width) - 0.5;
+                //set ball speed
+                ball.SetSpeed(hitPercent);
+            }
+
+            //ball and blocks
+            foreach(Block block in blocks)
+            {
+                Rect ballRect = ball.GetRect();
+                ballRect.Intersect(block.GetRect());
+                if(!ballRect.IsEmpty)
+                {
+                    blocks.Remove(block);
+                    //remove From Canvas
+                    canvas.Children.Remove(block);
+                    //SpeedX and SpeedY
+                    double ballCenter = ball.LocationX + (ball.Width) / 2;
+                    if (ballCenter < block.LocationX || ballCenter > (block.LocationX + block.Width))
+                    {
+                        ball.SpeedX *= -1;
+                    }
+                    else
+                    {
+                        ball.SpeedY *= -1;
+                    }
+                    break;
+                        
+                }
+            }
+        }
+    }
+}
